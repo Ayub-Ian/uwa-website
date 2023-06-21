a8bfc1e4cc7f74524a01a143c63c6f46a822f5ba
Ayub Ian <iayub.mia@gmail.com>
2023-06-22 00:24:44 +0300

    Added header to layout

---
+++ app/(site)/layout.js
@@ -0,0 +1,39 @@
+import "../globals.css";
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+
+const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
+
+export const metadata = {
+  title: "Ushauri Wellness Africa",
+  description:
+    "Non-profit organization dedicated to promoting mental health, drug and substance awareness, financial literacy and civic education",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={`${montserrat.className} tw-py-2.5`}>
+        <header className="tw-py-1">
+          <div className="tw-px-4">
+          <div className="tw-flex tw-items-center">
+            <Image
+              src="/logo.png"
+              alt="Ushauri Wellness Africa logo"
+              width={61}
+              height={42}
+            />
+            <h1 className="tw-text-[#B12D68] tw-font-extrabold tw-text-xl tw-hidden md:tw-block ">
+              Ushauri Wellness Africa
+            </h1>
+          </div>
+          </div>
+        </header>
+        {children}
+      </body>
+    </html>
+  );
+}
