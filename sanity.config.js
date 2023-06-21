c06206745d0bb3390e53ab562151384b1c005c82
Ayub Ian <iayub.mia@gmail.com>
2023-06-21 21:39:19 +0300

    first commit

---
+++ sanity.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import schemas from "./sanity/schemas"
+
+const config = defineConfig({
+  projectId: "pc1y0wgw",
+  dataset: "production",
+  title: "Ushauri Wellness Africa Website",
+  apiVersion: "2023-06-21",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas }
+})
+
+export default config
