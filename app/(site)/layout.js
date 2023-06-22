6072d0de09e796b68e8f2cd242ce7efa80c5db46
Ayub Ian <iayub.mia@gmail.com>
2023-06-22 10:24:49 +0300

    Added footer

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -1,6 +1,7 @@
 import "../globals.css";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
+import Footer from "../components/footer";
 
 const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
 
@@ -12,27 +13,31 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="tw-dark">
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${montserrat.className} tw-py-2.5`}>
         <header className="tw-py-1">
           <div className="tw-px-4">
-          <div className="tw-flex tw-items-center">
-            <Image
-              src="/logo.png"
-              alt="Ushauri Wellness Africa logo"
-              width={61}
-              height={42}
-            />
-            <h1 className="tw-text-[#B12D68] tw-font-extrabold tw-text-xl tw-hidden md:tw-block ">
-              Ushauri Wellness Africa
-            </h1>
-          </div>
+            <div className="tw-flex tw-items-center">
+              <Image
+                src="/logo.png"
+                alt="Ushauri Wellness Africa logo"
+                width={61}
+                height={42}
+              />
+              <h1 className="tw-text-[#B12D68] tw-font-extrabold tw-text-xl tw-hidden md:tw-block ">
+                Ushauri Wellness Africa
+              </h1>
+            </div>
           </div>
         </header>
-        {children}
+        <main className="tw-px-2.5 tw-min-h-screen tw-flex tw-flex-col ">{children}
+        <Footer />
+        </main>
+
+        
       </body>
     </html>
   );
--- app/(site)/page.js
+++ app/(site)/page.js
@@ -1,10 +1,19 @@
-import { getPrograms } from "@/sanity/sanity-utils"
+import { getPrograms } from "@/sanity/sanity-utils";
 
 export default async function Home() {
-
   const programs = await getPrograms();
   // console.log(programs)
   return (
-    <div>Projects go here</div>
-  )
+    <div className="tw-w-full">
+      <div className="tw-bg-[#A6D49F] tw-rounded-md tw-px-4 tw-py-32">
+        <h1 className=" tw-text-black tw-font-bold tw-text-xl">
+          Open conversations for a better future
+        </h1>
+        <h3 className="tw-text-black">
+          Mobilising Conversations: Creating Safe Spaces for Critical
+          Discussions
+        </h3>
+      </div>
+    </div>
+  );
 }
